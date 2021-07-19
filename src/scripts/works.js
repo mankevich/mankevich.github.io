5d53d89ebeae11e88f8f32f876fc4bb10f1b5b8c
Mankevich Victor <jumperfromcloset@gmail.com>
2021-07-19 15:09:31 +0700

    all sliders and buttons

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -1,25 +1,40 @@
 import Vue from "vue"
 
 const thumbs = {
+  props: ["works", "currentWork"],
   template: "#preview-thumbs"
 };
 
 const btns = {
-  template: "#preview-btns"
+  template: "#preview-btns",
 };
 
 const display = {
+  props: ["currentWork", "works", "currentIndex"],
   template: "#preview-display",
-  components: { thumbs, btns }
+  components: { thumbs, btns },
+  computed: {
+    reversedWorks() {
+      const works = [...this.works];
+      return works.slice(0, 3).reverse();
+    }
+  }
 };
 
 const tags = {
+  props: ["tags"],
   template: "#preview-tags"
 };
 
 const info = {
+  props: ["currentWork"],
   template: "#preview-info",
-  components: { tags }
+  components: { tags },
+  computed: {
+    tagsArray() {
+      return this.currentWork.skills.split(",");
+    }
+  }
 };
 
 new Vue({
@@ -28,16 +43,52 @@ new Vue({
   components: { display, info },
   data() {
     return {
-      works: []
+      works: [],
+      currentIndex: 0
+    }
+  },
+  computed: {
+    currentWork() {
+      return this.works[0];
+    }
+  },
+  watch: {
+    currentIndex(value) {
+      this.makeInfiniteLoopForNdx(value);
     }
   },
   methods: {
+    makeInfiniteLoopForNdx(index) {
+      const worksNumber = this.works.length - 1;
+      if(index < 0) this.currentIndex = worksNumber;
+      if(index > worksNumber) this.currentIndex = 0;
+    },
     requireImagesToArray(data) {
       return data.map(item => {
         const requiredImage = require(`../images/content/${item.photo}`).default;
         item.photo = requiredImage;
         return item;
-      })
+      });
+    },
+    slide(direction) {
+      const lastItem = this.works[this.works.length - 1];
+      const itemsCount = this.works.length - 1;
+      switch(direction){
+        case "next":
+          if(this.currentIndex < itemsCount) {
+            this.works.push(this.works[0]);
+            this.works.shift();
+            this.currentIndex++;
+          }          
+          break;
+        case "prev":
+          if(this.currentIndex > 0) {
+            this.works.unshift(lastItem);
+            this.works.pop();
+            this.currentIndex--;
+          }
+          break;
+      }
     }
   },
   created() {
